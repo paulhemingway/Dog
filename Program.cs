dbfdba847970e79e732236168cdd76e26fcd5aec
Paul Hemingway <phemingway@ymail.com>
2020-02-13 13:32:21 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
